123d1c2324ad5c0cec4b2e2d05bc8885346a5096
Carlos Morera Gomez <carlosmorera1565@gmail.com>
2017-03-21 14:52:13 -0500

    PARTE 2.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,32 +1,51 @@
-var theObject= null;
+var theObject = null;
+var canvas = null;
+var x = 0;
+var y = 0;
+
 
 var stompClient = null;
 
 function connect() {
     var socket = new SockJS('/stompendpoint');
-    function foo(){
-       console.log('foo fue llamada!');
+    function foo() {
+        console.log('foo fue llamada!');
     }
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/newpoint', function (data) { 
-          theObject=JSON.parse(data.body);
-          callback(theObject);
+        stompClient.subscribe('/topic/newpoint', function (data) {
+            theObject = JSON.parse(data.body);
+            callback(theObject);
         });
-        
+
     });
 }
 
-function callback(theObject){
-    alert("X:"+theObject["x"]+"Y:"+theObject["y"]);    
+function callback(theObject) {
+    //alert("X:" + theObject["x"] + "Y:" + theObject["y"]);
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(theObject["x"], theObject["y"], 1, 0, 2 * Math.PI);
+    ctx.stroke();
 }
 
 
 sendPoint = function () {
-    stompClient.send("/topic/newpoint", {}, JSON.stringify({x:10,y:10}));
-    
+    stompClient.send("/topic/newpoint", {}, JSON.stringify({x: x, y: y}));
+
 };
+function getMousePos(canvas, evt) {
+var rect = canvas.getBoundingClientRect();
+        return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+        };
+        
+        }
+
+
 
 
 function disconnect() {
@@ -38,10 +57,21 @@ function disconnect() {
 }
 
 
+
 $(document).ready(
         function () {
-            connect();
-            console.info('connecting to websockets');
+        connect();
+        console.info('connecting to websockets');
+        var canvas = document.getElementById('myCanvas');
+        var context = canvas.getContext("2d");
+        canvas.addEventListener('mousedown', function (evt) {     
+            var mousePos = getMousePos(canvas, evt);
+        
+               x = mousePos.x;
+               y = mousePos.y;
+               sendPoint();
+                var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+               
+            }, false);
 
-        }
-);
+        });
