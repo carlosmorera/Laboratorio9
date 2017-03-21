0e5e49244115e60bb1285d97ced481dfb2dd7aca
Carlos Morera Gomez <carlosmorera1565@gmail.com>
2017-03-21 14:46:35 -0500

    PARTE 1

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,17 +1,33 @@
+var theObject= null;
+
 var stompClient = null;
 
 function connect() {
     var socket = new SockJS('/stompendpoint');
+    function foo(){
+       console.log('foo fue llamada!');
+    }
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         console.log('Connected: ' + frame);
-        
-        stompClient.subscribe('/topic/TOPICOXX', function (data) {
-           
-            
+        stompClient.subscribe('/topic/newpoint', function (data) { 
+          theObject=JSON.parse(data.body);
+          callback(theObject);
         });
+        
     });
 }
+
+function callback(theObject){
+    alert("X:"+theObject["x"]+"Y:"+theObject["y"]);    
+}
+
+
+sendPoint = function () {
+    stompClient.send("/topic/newpoint", {}, JSON.stringify({x:10,y:10}));
+    
+};
+
 
 function disconnect() {
     if (stompClient != null) {
